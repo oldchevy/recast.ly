35f9575dc7062354e953c0ff7b8ac477f488fce9
oldchevy <nick.olszowy@gmail.com>
2016-08-13 11:21:24 -0700

    Implement search bar functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.throttled = _.throttle(this.props.searchYouTube, 501);
+
     this.state = {
       playlist: window.exampleVideoData,
       currentVideo: window.exampleVideoData[0]
     };
   }
+
+  componentDidMount() {
+    this.props.searchYouTube({
+      key: window.YOUTUBE_API_KEY,
+      query: 'cats',
+      max: 10
+    }, this.rerender.bind(this));
+  }
+
+  rerender(result) {
+    this.setState({
+      playlist: result
+    });
+  }
+
   render() {
-    // debugger;
     return (
       <div>
-        <Nav />
+        <Nav rerender={this.rerender.bind(this)} searchYouTube={this.throttled} />
         <div className="col-md-7">
           <VideoPlayer video={this.state.currentVideo}/>
         </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,23 @@
-var Search = () => (
+var Search = (props) => {
+
+  var search = function(event) {
+    var options = {
+      key: window.YOUTUBE_API_KEY,
+      query: $(event.target).val(),
+      max: 10 
+    };
+    props.searchYouTube(options, props.rerender);
+  };
+
+  return (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
+    <input className="form-control" type="text" onChange={search}/>
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
   </div> 
-);
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
